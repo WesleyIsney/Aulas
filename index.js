b01114f43094423387dcdfd9f7938d6418f66c10
WesleyIsney <110308030+WesleyIsney@users.noreply.github.com>
2023-05-16 20:23:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ import Express from "express";
 import { dbContatos } from "./Atividades_Aula/1_Aula/Contato.js"
 
 const contatos = dbContatos;
-console.log(contatos);
+const app = Express();
+
+app.get('/', (req, res) =>{
+    res.send(contatos);
+});
+app.listen(3000, () => {
+    console.log('Servidor iniciado na porta 3000');
+  });
+
+export default app;
+
+
+
+
+
